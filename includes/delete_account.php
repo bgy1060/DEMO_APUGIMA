c7ca3afa4e0b03f8a3181786205a4d871528e03f
eun-g-kim <eunji9803@gmail.com>
2020-11-14 16:27:36 +0900

    transaction

--- includes/delete_account.php
+++ includes/delete_account.php
@@ -1,10 +1,24 @@
 <?php
 	include_once 'dbh.inc.php';
-	$sql = "DELETE FROM users WHERE user_id='$_GET[id]';";
-	$result = mysqli_query($conn, $sql);
-	if ($result)
-		echo "<script>alert('Your account is successfully deleted.');</script>";
-	else
-		echo "<script>alert('Error occured. Try again.');</script>";
+	mysqli_query($conn, "BEGIN");
+	$sqls = ["DELETE FROM hospital_reviews WHERE uid='$_GET[id]';",
+						"DELETE FROM medicine_reviews WHERE uid='$_GET[id]';",
+						"DELETE FROM prescriptions WHERE uid='$_GET[id]';",
+						"DELETE FROM diaries WHERE uid='$_GET[id]';",
+						"DELETE FROM users WHERE uid='$_GET[id]';"];
+
+	for ($i = 0; $i < count($sqls); $i++)
+			if (!mysqli_query ($conn, $sqls[$i]))
+					break;
+
+	if ($i == count($sqls)){
+			mysqli_query($conn, "COMMIT");
+			echo "<script>alert('Your account is successfully deleted.');</script>";
+	}
+	else {
+			mysqli_query($conn, "ROLLBACK");
+			echo "<script>alert('Error occured. Try again.');</script>";
+	}
+
 ?>
 <meta http-equiv="refresh" content="0;url='../index.php'">
